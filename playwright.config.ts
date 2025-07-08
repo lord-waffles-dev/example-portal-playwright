1e39cdf7cc89aca2c4e7665546eebddd5aeee593
maxwell <maxwell.leslie@recurohealth.com>
2025-07-08 12:20:52 -0500

    fixing lint errors

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig } from '@playwright/test';
 export default defineConfig({
   // Only use this testMatch when running Playwright directly, not with Cucumber
-  testMatch: process.env.PLAYWRIGHT_TEST_MATCH ? "tests/playwright/exploratory.test.ts" : "",
+  testMatch: process.env.PLAYWRIGHT_TEST_MATCH ? 'tests/playwright/exploratory.test.ts' : '',
   use: {
-    headless: false,
-  },
+    headless: false
+  }
 });
