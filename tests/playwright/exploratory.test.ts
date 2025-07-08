e2aeb2ea5e75dfc68930ee09f9fc7c7895f7dc19
maxwell <maxwell.leslie@recurohealth.com>
2025-07-08 11:55:55 -0500

    Fix path for network capture

--- tests/playwright/exploratory.test.ts
+++ tests/playwright/exploratory.test.ts
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test';
-import { setupNetworkCapture } from '../utils/networkCapture';
+import { setupNetworkCapture } from '../../src/utils/networkCapture';
 test('Manual exploration with network capture', async ({ page }) => {
   await setupNetworkCapture(page);
   await page.goto('https://cerebro-staging.recurohealth.com/login.aspx');
